6427e77402a983d7b683d6b9339e07730eef3fb0
Josef Čáslavský <josef.caslavsky@neogenia.cz>
2018-06-12 08:58:57 +0200

    tests update

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,9 +3,4 @@ use Tester\Assert;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-Assert::false(true);
-Assert::false(true);
-Assert::false(true);
-Assert::false(true);
-Assert::false(true);
-Assert::false(true);
+Assert::true(true);
