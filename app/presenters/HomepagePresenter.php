eeecd27be5c4ddbef532c8da8684be2cf157615d
Josef Čáslavský <josef.caslavsky@neogenia.cz>
2018-06-16 00:16:20 +0200

    typo

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -1,11 +1,9 @@
 <?php
 namespace App\Presenters;
-
 use Nette;
-
 class HomepagePresenter extends Nette\Application\UI\Presenter {
 	public function actionTest() {
-		$this->template->title = 'test';
+		$this->template->title='test';
 		$this->template->description = 'test';
 		$this->template->aaa = 'aaa';
 //		$this->template->xxx = 'xxx';
