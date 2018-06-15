91d604f759e6e9656d2274346796e71f5b2e7d4d
Josef Čáslavský <josef.caslavsky@neogenia.cz>
2018-06-15 23:33:15 +0200

    code sniffer test

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -1,17 +1,23 @@
 <?php
-
 namespace App\Presenters;
 
 use Nette;
 
-
-class HomepagePresenter extends Nette\Application\UI\Presenter
-{
-	public function actionTest()
-	{
+class HomepagePresenter extends Nette\Application\UI\Presenter {
+	public function actionTest() {
 		$this->template->title = 'test';
 		$this->template->description = 'test';
 		$this->template->aaa = 'aaa';
 //		$this->template->xxx = 'xxx';
+
+		return;
+
+		$this->template->aaa = 'aaa';
+
+		if (false) {
+
+		} else {
+
+		}
 	}
 }
